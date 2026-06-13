ff18e8cf773423d2803e4224539c3cb6eee4b259
haidar3345 <haidaralfaruq23@student.cs.unida.gontor.ac.id>
2026-06-13 18:56:09 +0700

    Add files via upload

---
+++ jetpack compose/main/java/com/example/jetpackcompose/SampleData.kt
@@ -0,0 +1,75 @@
+package com.example.jetpackcompose
+
+/**
+ * SampleData for Jetpack Compose Tutorial 
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "HAIDAR",
+            "Test... Test... Test..."
+        ),
+        Message(
+            "HAIDAR",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trimMargin()
+        ),
+        Message(
+            "HAIDAR",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trimMargin()
+        ),
+        Message(
+            "HAIDAR",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "HAIDAR",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trimMargin()
+        ),
+        Message(
+            "HAIDAR",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "HAIDAR",
+            "Writing Kotlin for UI seems so natural."
+        ),
+        Message(
+            "HAIDAR",
+            "Android Studio next to Me!"
+        ),
+        Message(
+            "HAIDAR",
+            "A lot of things are happening in the Android world!"
+        ),
+        Message(
+            "HAIDAR",
+            "I've been usage Compose for a while now and it's awesome."
+        ),
+        Message(
+            "HAIDAR",
+            "It's much cleaner than XML."
+        ),
+        Message(
+            "HAIDAR",
+            "I'm excited to see where it goes!"
+        ),
+        Message(
+            "HAIDAR",
+            "Keep it up!"
+        ),
+    )
+}
